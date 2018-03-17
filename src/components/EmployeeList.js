1fdc5546597995377b5abb2ef42e4d1f44b8431f
EHenry92 <EHenry692@hotmail.com>
2018-03-17 09:19:57 -0400

    show list

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,18 +1,47 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, ListView} from 'react-native';
 import {connect} from 'react-redux';
-import {employeesFetch} from '../actions'
+import {employeesFetch} from '../actions';
+import ListItem from './ListItem';
 
 class EmployeeList extends Component {
   componentWillMount() {
     this.props.employeesFetch();
+    this.createDataSource(this.props);
+  }
+  componentWillReceiveProps(nextProps){
+    //access to nextProps (new) and this.Props (old)
+    this.createDataSource(nextProps);
+  }
+  createDataSource({list}) {
+    //gets called with props
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+    this.dataSource = ds.cloneWithRows(list)
+  }
+  renderRow(employee) {
+    return <ListItem employee = {employee} />
   }
 
   render () {
+    console.log(this.props.list)
     return(
-      <View>
-      </View>
+      <ListView
+        enableEmptySections
+        dataSource = {this.dataSource}
+        renderRow = {this.renderRow}
+      >
+      </ListView>
     )
   }
 }
-export default connect(null, employeesFetch)(EmployeeList);
+
+const mapState = (state) => {
+  const list = _.map(state.employeeList, (val, uid) =>{
+    return {...val, uid}
+  });
+  return {list};
+};
+export default connect(mapState, {employeesFetch})(EmployeeList);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,7 @@ import {combineReducers} from 'redux';
 import auth from './auth';
 import employeeForm from './employeeForm';
 import employeeList from './employeeList';
+
 export default combineReducers({
   auth, employeeForm, employeeList
 });
